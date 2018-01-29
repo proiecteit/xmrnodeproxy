dd708d75d1ca716a91a365b8ebbeb3ee74bf1e12
proiecteit <35568774+proiecteit@users.noreply.github.com>
2018-01-29 19:09:18 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION=$1
+if [ -z "$VERSION" ]; then
+    echo "Building: latest"
+    TAG=latest
+else
+    echo "Building: $VERSION"
+    TAG=$VERSION
+fi
+
+# Build
+docker build -t proiecteit/xmrnodeproxy:${TAG} .
+
+# Push
+docker push proiecteit/xmrnodeproxy:${TAG}
